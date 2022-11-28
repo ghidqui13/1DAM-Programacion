db6958453d445c93b766db95d5dc9f694dc4c8ff
Gabriel Hidalgo <114946438+ghidqui13@users.noreply.github.com>
2022-11-28 20:46:46 +0100

    Add files via upload

---
+++ 02-EjerciciosT2/Ejercicio29.java
@@ -0,0 +1,53 @@
+
+package ejercicio29;
+import java.util.*;
+import bpc.daw.objetos.*;
+/**
+ *
+ * @author Gabriel Hidalgo <hidalgoquinterogabriel@gmail.com>
+ */
+public class Ejercicio29 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        try{
+            System.out.println("Escribe un un numero:");
+        int n = new Scanner(System.in).nextInt();
+        
+        }catch(Exception e){
+        
+            System.out.println("Error InputMismatchException");
+        }
+        try{
+            System.out.println("Escribe los argumentos:");
+            System.out.println("Argumento 1: "+ args[0]);
+            System.out.println("Argumento 1: "+ args[1]);
+        
+            int n1 = Integer.parseInt(args[0]);
+            int n2 = Integer.parseInt(args[1]);
+        
+            int n3 = n1 + n2;
+        
+            System.out.println("La suma de los argumentos es: " +n3);
+        
+        
+        }catch(Exception e){
+        
+            System.out.println("Si no recibe los argumentos dará ArrayIndexOutOfBoundsException");
+        }
+        try{
+            System.out.println("Apartado C");
+            Caja c = null;
+            c.abrirCaja();   
+            System.out.println(c.getMensaje().length());
+        }catch(Exception e){
+            
+        System.out.println("Si intentamos leer una caja vacia dará NullPointerException");
+        }
+        
+    }
+
+}
