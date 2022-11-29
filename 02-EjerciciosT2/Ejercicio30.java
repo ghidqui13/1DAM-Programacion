1b3debfc40502e9491172fc1de412cdcc83234aa
Gabriel Hidalgo <114946438+ghidqui13@users.noreply.github.com>
2022-11-29 16:03:20 +0100

    Update Ejercicio30.java

--- 02-EjerciciosT2/Ejercicio30.java
+++ 02-EjerciciosT2/Ejercicio30.java
@@ -13,13 +13,18 @@ public class Ejercicio30 {
     public static void main(String[] args) {
         
         System.out.println("SQLException:");
-        System.out.println("Es una RuntimeException que se genera cuando los datos leídos de la base de datos o los datos que se van a escribir en la base de datos son incorrectos.\n");
+        System.out.println("Es una RuntimeException que se genera cuando "
+              + "los datos leídos de la base de datos o los datos que se van a "
+              + "escribir en la base de datos son incorrectos.\n");
         
         System.out.println("IllegalArgumentException :");
-        System.out.println("Es una RuntimeException que indica que un método ha recibido un argumento que no es válido o es inadecuado para los fines de este método.\n");
+        System.out.println("Es una RuntimeException que indica "
+              + "que un método ha recibido un argumento que no es "
+              + "válido o es inadecuado para los fines de este método.\n");
         
         System.out.println("IllegalStateException");
-        System.out.println("Es una RuntimeException que indica que se llama a un método en un momento ilegal o inapropiado.");
+        System.out.println("Es una RuntimeException que indica "
+              + "que se llama a un método en un momento ilegal o inapropiado.");
     }
 
 }
